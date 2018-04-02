d6f1dad8f20e389628cc86b0b10d0ed910a62aef
aalok.sathe <aalok.sathe@gmail.com>
2018-04-02 14:06:10 -0400

    modified:   Person.cpp 	new file:   tester.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,5 +1,11 @@
 // This file coded by as9kc
 // as9kc and hn3gn
 
+#ifndef __PERSON_CPP__
+#define __PERSON_CPP__
+
 #include "Person.h"
 
+
+
+#endif
---
+++ tester.cpp
@@ -0,0 +1,11 @@
+// as9kc and hn3gn
+#include <iostream>
+#include "Person.h"
+#include "Student.h"
+
+int main()
+{
+
+    
+	return 0;
+}
